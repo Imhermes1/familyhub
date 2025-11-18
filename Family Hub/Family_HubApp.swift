4f9cda8f1d25528ee6dfe78bcaa697b15604ff21
imHermes1 <lukeforn@gmail.com>
2025-11-18 14:13:38 +1100

    Initial commit

---
+++ Family Hub/Family_HubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Family_HubApp.swift
+//  Family Hub
+//
+//  Created by Luke Fornieri on 18/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct Family_HubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
